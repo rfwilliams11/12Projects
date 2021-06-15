985d2e4c7a6f42835b8761b050b34de9c456f28e
WilliamsR <WilliamsR@CalSAWS.org>
2021-06-14 22:13:35 -0700

    initial commit with 5 projects

---
+++ Madlibs/madlib.py
@@ -0,0 +1,14 @@
+youtuber = "Kylie Ying"
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Another Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
