42945b2b65c31d39817a90e030f9b4eacb08e24f
WilliamsR <WilliamsR@CalSAWS.org>
2021-06-16 14:48:25 -0700

    finalizing Tic Tac Toe game

--- Tic Tac Toe/game.py
+++ Tic Tac Toe/game.py
@@ -1,22 +1,25 @@
 import math
 import time
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
 
 
-class TicTacToe:
+class TicTacToe():
     def __init__(self):
-        self.board = [' ' for _ in range(9)]  # we will use a single list to rep 3x3 board
-        self.current_winner = None  # keep track of winner
+        self.board = self.make_board()
+        self.current_winner = None
+
+    @staticmethod
+    def make_board():
+        return [' ' for _ in range(9)]
 
     def print_board(self):
-        # this is just getting the rows
-        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
-            print(' | ' + ' | '.join(row) + ' |')
+        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2
-        number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
@@ -28,24 +31,15 @@ class TicTacToe:
             return True
         return False
 
-    def available_moves(self):
-        return [i for i, x in enumerate(self.board) if x == " "]
-
-    def empty_squares(self):
-        return ' ' in self.board
-
-    def num_empty_squares(self):
-        return self.board.count(' ')
-
     def winner(self, square, letter):
         # check the row
         row_ind = math.floor(square / 3)
-        row = self.board[row_ind * 3:(row_ind + 1) * 3]
+        row = self.board[row_ind*3:(row_ind+1)*3]
         # print('row', row)
         if all([s == letter for s in row]):
             return True
         col_ind = square % 3
-        column = [self.board[col_ind + i * 3] for i in range(3)]
+        column = [self.board[col_ind+i*3] for i in range(3)]
         # print('col', column)
         if all([s == letter for s in column]):
             return True
@@ -60,7 +54,18 @@ class TicTacToe:
                 return True
         return False
 
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def available_moves(self):
+        return [i for i, x in enumerate(self.board) if x == " "]
+
+
 def play(game, x_player, o_player, print_game=True):
+
     if print_game:
         game.print_board_nums()
 
@@ -89,8 +94,9 @@ def play(game, x_player, o_player, print_game=True):
         print('It\'s a tie!')
 
 
+
 if __name__ == '__main__':
-    x_player = RandomComputerPlayer('X')
+    x_player = SmartComputerPlayer('X')
     o_player = HumanPlayer('O')
     t = TicTacToe()
     play(t, x_player, o_player, print_game=True)
